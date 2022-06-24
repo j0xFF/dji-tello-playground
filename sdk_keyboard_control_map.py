47fcb5b2ac370f68e7f8c6bff8aa265713dca459
J Lleó <j0xFF@users.noreply.github.com>
2022-06-24 22:37:37 +0200

    [ADD] Keyboard control SDK implementation. Mapping

---
+++ sdk_keyboard_control_map.py
@@ -0,0 +1,62 @@
+# DJI Tello SDK
+# Keyboard control - Manual
+# Mapping added
+
+from djitellopy import tello
+from time import sleep
+import base_keypress as bkp
+
+"""
+fwd_spd = 117/10 in cm/s (15cm/s)
+ang_spd = 360/10 in deg/s
+int = 0.25
+dst_int = fwd_spd * int
+ang_int = ang_spd * int
+"""
+
+bkp.init()
+leb = tello.Tello()
+leb.connect()
+print(leb.get_battery())
+
+
+def get_keyboard_input():
+    fbs, rlt, upd, yaw = 0, 0, 0, 0
+    spd = 50
+    ito = False
+
+    if bkp.get_key('UP'):
+        fbs = spd
+    elif bkp.get_key('DOWN'):
+        fbs = -spd
+
+    if bkp.get_key('RIGHT'):
+        rlt = spd
+    elif bkp.get_key('LEFT'):
+        rlt = -spd
+
+    if bkp.get_key('w'):
+        upd = spd
+    elif bkp.get_key('s'):
+        upd = -spd
+
+    if bkp.get_key('a'):
+        yaw = -spd
+    elif bkp.get_key('d'):
+        yaw = spd
+
+    if bkp.get_key('SPACE') and not ito:
+        leb.takeoff()
+        ito = True
+
+    if bkp.get_key('SPACE') and ito:
+        leb.land()
+        ito = False
+
+    return [fbs, rlt, upd, yaw, ito]
+
+
+while True:
+    vals = get_keyboard_input()
+    leb.send_rc_control(vals[0], vals[1], vals[2], vals[3])
+    sleep(0.05)
